0c2a6977b575aaf6079b5719f6e594aa59936c61
Syahrun12 <97746302+Syahrun12@users.noreply.github.com>
2022-08-31 12:38:03 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -437,7 +437,7 @@ module.exports = {
                     if (m.chat in global.db.data.chats || m.sender in global.db.data.users) {
                         let chat = global.db.data.chats[m.chat]
                         let user = global.db.data.users[m.sender]
-                        if (!['verify.js', 'creator.js'].inludes(name) && user && user.verifikasi === false) return conn.sendButtonImg(m.chat, img, `Hallo {'@' + m.sender.split`@`[0]}, Kamu Belum Terdaftar DI Database Bot\nSilahkan Daftar Terebih Dahulu\nSilahkan Klik Button DIbawah Ini Untuk Terdaftar Ke Database Bot`.trim(), wm, 'Verify', '@verify', m)                        
+                        if (name != 'verify.js' && user && user.verifikasi === false) return conn.sendButtonImg(m.chat, img, `Hallo {'@' + m.sender.split`@`[0]}, Kamu Belum Terdaftar DI Database Bot\nSilahkan Daftar Terebih Dahulu\nSilahkan Klik Button DIbawah Ini Untuk Terdaftar Ke Database Bot`.trim(), wm, 'Verify', '@verify', m)                        
                         if (!['unbanchat.js', 'dompet.js', 'creator.js'].includes(name) && chat && chat?.isBanned && !isPrems) return // Kecuali ini, bisa digunakan
                         if (!['unbanchat.js', 'dompet.js', 'creator.js'].includes(name) && user && user?.banned) return
                         
