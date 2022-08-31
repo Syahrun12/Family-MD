6b859cbaac09b891b512d3af956288a69ba135d9
Syahrun12 <97746302+Syahrun12@users.noreply.github.com>
2022-08-31 13:06:41 +0700

    Update verify.js

--- plugins/verify.js
+++ plugins/verify.js
@@ -3,7 +3,7 @@ let PhoneNumber = require('awesome-phonenumber')
 let handler = async function (m, { text, usedPrefix }) {
   let user = global.db.data.users[m.sender]
   if (user.verifikasi === true) throw `Anda Sudah Terdaftar Ke Dalam Database`
-conn.sendButton(m.chat, '```Sukses Verify```', m)
+conn.sendButton(m.chat, '```Sukses Verify```', wm, 'Menu', '.menu', m)
     conn.reply('6285669537589-1614830865@g.us', `*「 Verify 」*\nNomor : wa.me/${m.sender.split`@`[0]}\nPesan : ${'@' + m.sender.split`@`[0]} Berhasil Melakukan Verifikasi`, m)
 }
 }
